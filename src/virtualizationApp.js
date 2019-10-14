39164c9631e44add42e30f336ecfaf06d4bfd6b8
horwardL <hwloh14@gmail.com>
2019-10-13 22:27:29 -0400

    added virtualization app component

---
+++ src/virtualizationApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import NavBar from "./components/Navbar/navbar.jsx";
+
+class VirtualizationApp extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+      </React.Fragment>
+    );
+  }
+}
+
+export default VirtualizationApp;

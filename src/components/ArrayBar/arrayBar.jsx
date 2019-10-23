b5bc0e5ffeb127458fba85df88784399060eb4ce
horwardL <hwloh14@gmail.com>
2019-10-22 20:51:23 -0400

    connected array size slider with array components

--- src/components/ArrayBar/arrayBar.jsx
+++ src/components/ArrayBar/arrayBar.jsx
@@ -6,6 +6,18 @@ const PRIMARY_COLOR = "white";
 
 export default class ArrayBar extends Component {
   render() {
+    const { array, size } = this.props;
+    let screenWidth = window.screen.width;
+    let barWidth = screenWidth / (size * 1.5);
+    if (screenWidth >= 768) {
+      screenWidth -= 244;
+      barWidth = (window.screen.width - 444) / (size * 2.3);
+    }
+    console.log(barWidth);
+    let widthStyle = {
+      width: `${screenWidth}px`
+    };
+
     return (
       <div className="arr-container" style={widthStyle}>
         <div className="array" style={widthStyle}>
